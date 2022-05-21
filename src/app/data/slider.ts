13b80022d60e87cfc9dfc0e6285cf5889af6668b
Nicolás Cabello <cabellonic@gmail.com>
2022-05-21 16:33:13 -0300

    update: Add slider data

---
+++ src/app/data/slider.ts
@@ -0,0 +1,49 @@
+export const slider = [
+	{
+		id: 1,
+		title: 'Kenshi Yonezu lanza su nuevo single "M87"',
+		description:
+			'Luego de publicar "POP SONG" el cantante japonés lanzó su nuevo single "M87", que contiene una canción homónima que es el tema principal de la película "Shin Ultraman", dirigida por Hideaki Anno.',
+		image:
+			'https://images.ctfassets.net/7wbtbtnd48mu/3vCN5kxUBVKKblNaUNAgUE/e440b6fb263d3654fc6e775094a83c2c/M87.webp?w=1500&h=1500&q=50&fm=webp',
+		button: 'Seguir leyendo',
+		link: '#',
+	},
+	{
+		id: 2,
+		title: 'Entrevista sobre "STRAY SHEEP" con Natalie Music',
+		image:
+			'https://images.ctfassets.net/7wbtbtnd48mu/XTDhqG8GZGz6MNV7brvdG/e8cc8cea5129f3ca704c6c9bb91465fa/cover.webp',
+		link: '#',
+	},
+	{
+		id: 3,
+		title:
+			'"POP SONG", el resultado de una colaboración entre Kenshi Yonezu y PlayStation',
+		description:
+			'Finalmente el video musical de "POP SONG", una canción que Kenshi Yonezu compuso para un comercial de PlayStation, ha sido lanzado.',
+		image:
+			'https://images.ctfassets.net/7wbtbtnd48mu/3FAgSDrKUEb9ViPoI1EsGx/82cfbb672a1debffdf36d00d9f0436bc/POP_SONG.webp',
+		button: 'Ver video',
+		link: '#',
+	},
+	{
+		id: 4,
+		title: 'Kenshi Yonezu desembarca en TikTok',
+		description:
+			'Este 25 de junio Kenshi Yonezu se sumó a la popular plataforma TikTok bajo el nombre de usuario @kenshiyonezu_08.',
+		image:
+			'https://images.ctfassets.net/7wbtbtnd48mu/397BV2COCrAHjVRdpxU512/cc90858b39dea9260e0bf4b91c4618a2/TikTok.webp',
+		button: 'Ir a su perfil',
+		link: '#',
+	},
+	{
+		id: 5,
+		title:
+			'Kenshi Yonezu culminó su quinto álbum después de dos años y nueve meses de haber lanzado "BOOTLEG".',
+		image:
+			'https://images.ctfassets.net/7wbtbtnd48mu/mInMVtN401AemzBNIK6C1/1738f8d02b8233f24321ac477df7d53d/Natalie_-_STRAY_SHEEP_-_01.webp',
+		button: 'Leer noticia',
+		link: '#',
+	},
+];
