da074573011a6b9644daa4c1a0171e877354c467
Nicolás Cabello <cabellonic@gmail.com>
2022-05-29 15:53:59 -0300

    refactor: Replace Menu dummy data with fetched one. Add MenuItems placeholder

--- src/layout/public/components/Header/MainMenu/MenuItems.tsx
+++ src/layout/public/components/Header/MainMenu/MenuItems.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+// Components
+import MenuItemsPlaceholder from './MenuItemsPlaceholder';
+// Hooks
+import { useMenu } from '@/common/hooks/useMenu';
 // Styles
 import styles from './MenuItems.module.scss';
 
 const MenuItems: React.FC = () => {
 	const location = useLocation();
-	const menu: any = []; // Temporal fix
+	const { menuItems, error, isLoading } = useMenu();
+
+	if (error) return <>Error al cargar el menu...</>;
+
+	if (!isLoading) return <MenuItemsPlaceholder />;
 
 	return (
 		<>
-			{menu.map(({ label, path }: { label: string; path: string }) => {
+			{menuItems.map(({ label, path }) => {
 				const isActive = location.pathname.includes(path);
 				return (
 					<Link
