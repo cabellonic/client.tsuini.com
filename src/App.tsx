4ba8bd5e83efa1dcb98deec11487f5f792b0dc8b
Nicolás Cabello <cabellonic@gmail.com>
2022-06-09 15:36:59 -0300

    Revert "fix: Correctly imports the App component"
    This reverts commit 7bc8119ad8365c0102f76308ae74a87b824f9b96.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 
-import UserProvider from './common/contexts/user.context';
-import TsuiniPlayer from '@components/TsuiniPlayer';
 import PublicRoutes from '@routes/public';
 import PrivateRoutes from '@routes/private';
 
 const App = () => {
+	const user = true;
+
 	return (
-		<UserProvider>
-			<Routes>
-				<Route path='/*' element={<PublicRoutes />} />
-				<Route path='/dashboard/*' element={<PrivateRoutes />} />
-			</Routes>
-			<TsuiniPlayer />
-		</UserProvider>
+		<Routes>
+			<Route path='/*' element={<PublicRoutes />} />
+			<Route path='/dashboard/*' element={<PrivateRoutes user={user} />} />
+		</Routes>
 	);
 };
 
