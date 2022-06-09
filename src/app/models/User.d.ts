576f37cd97d493cbe0010e6e56479473a45a6179
Nicolás Cabello <cabellonic@gmail.com>
2022-06-09 15:29:11 -0300

    update: Add User model

---
+++ src/app/models/User.d.ts
@@ -0,0 +1,12 @@
+export interface User {
+	provider: string;
+	id: string;
+	spotifyId: string;
+	spotifyPlan: string;
+	displayName: string;
+	avatar: string;
+	username: string;
+	email: string;
+	profileUrl: string;
+	rank: number;
+}
