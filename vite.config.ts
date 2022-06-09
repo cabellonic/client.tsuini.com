27d9a812737918095a4e7327f2ad4fede8152e48
Nicolás Cabello <cabellonic@gmail.com>
2022-06-09 15:33:38 -0300

    update: Add context path alias

--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,17 @@ export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'@': `${path.resolve(__dirname, 'src')}`,
-			'@layout': `${path.resolve(__dirname, 'src/layout')}`,
 			'@components': `${path.resolve(__dirname, 'src/common/components')}`,
+			'@contexts': `${path.resolve(__dirname, 'src/common/contexts')}`,
 			'@hooks': `${path.resolve(__dirname, 'src/common/hooks')}`,
-			'@services': `${path.resolve(__dirname, 'src/common/services')}`,
+			'@layout': `${path.resolve(__dirname, 'src/layout')}`,
 			'@models': `${path.resolve(__dirname, 'src/app/models')}`,
-			'@routes': `${path.resolve(__dirname, 'src/app/routes')}`,
-			'@styles': `${path.resolve(__dirname, 'src/app/styles')}`,
-			'@scss': `${path.resolve(__dirname, 'src/app/styles/variables')}`,
 			'@modules': `${path.resolve(__dirname, 'src/modules')}`,
 			'@pages': `${path.resolve(__dirname, 'src/pages')}`,
+			'@routes': `${path.resolve(__dirname, 'src/app/routes')}`,
+			'@scss': `${path.resolve(__dirname, 'src/app/styles/variables')}`,
+			'@services': `${path.resolve(__dirname, 'src/common/services')}`,
+			'@styles': `${path.resolve(__dirname, 'src/app/styles')}`,
 		},
 	},
 });
