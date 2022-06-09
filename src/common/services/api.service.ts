87a66815ac13a7d13b31577ae1a262ccebf72a15
Nicolás Cabello <cabellonic@gmail.com>
2022-06-09 15:58:43 -0300

    update: Add Authorization field and withCredentials option

--- src/common/services/api.service.ts
+++ src/common/services/api.service.ts
@@ -1,15 +1,30 @@
-import axios, { AxiosError } from 'axios';
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 axios.defaults.baseURL = import.meta.env.VITE_API_URI;
 
+type HeadersType = {
+	'Content-Type': string;
+	Authorization?: string;
+};
+
 const getHeaders = () => {
-	const headers = { 'Content-Type': 'application/json' };
+	const headers: HeadersType = {
+		'Content-Type': 'application/json',
+	};
+	const token = localStorage.getItem('accessToken');
+	if (token) headers.Authorization = `Bearer ${token}`;
 	return headers;
 };
 
-export const get = async (url: string) => {
+export const get = async (
+	url: string,
+	options: AxiosRequestConfig = { withCredentials: true }
+) => {
 	const headers = getHeaders();
 	try {
-		const { data } = await axios.get(url, { headers });
+		const { data } = await axios.get(url, {
+			headers,
+			...options,
+		});
 		return { data };
 	} catch (error) {
 		if (error instanceof AxiosError) return { error };
@@ -17,10 +32,17 @@ export const get = async (url: string) => {
 	}
 };
 
-export const post = async (url: string, body: any) => {
+export const post = async (
+	url: string,
+	body: any,
+	options: AxiosRequestConfig = { withCredentials: true }
+) => {
 	const headers = getHeaders();
 	try {
-		const { data } = await axios.post(url, body, { headers });
+		const { data } = await axios.post(url, body, {
+			headers,
+			...options,
+		});
 		return { data };
 	} catch (error) {
 		if (error instanceof AxiosError) return { error };
@@ -28,10 +50,17 @@ export const post = async (url: string, body: any) => {
 	}
 };
 
-export const update = async (url: string, body: any) => {
+export const update = async (
+	url: string,
+	body: any,
+	options: AxiosRequestConfig = { withCredentials: true }
+) => {
 	const headers = getHeaders();
 	try {
-		const { data } = await axios.put(url, body, { headers });
+		const { data } = await axios.put(url, body, {
+			headers,
+			...options,
+		});
 		return { data };
 	} catch (error) {
 		if (error instanceof AxiosError) return { error };
@@ -39,10 +68,16 @@ export const update = async (url: string, body: any) => {
 	}
 };
 
-export const remove = async (url: string) => {
+export const remove = async (
+	url: string,
+	options: AxiosRequestConfig = { withCredentials: true }
+) => {
 	const headers = getHeaders();
 	try {
-		const { data } = await axios.delete(url, { headers });
+		const { data } = await axios.delete(url, {
+			headers,
+			...options,
+		});
 		return { data };
 	} catch (error) {
 		if (error instanceof AxiosError) return { error };
