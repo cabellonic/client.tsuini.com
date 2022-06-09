c09013c8236bd8f16ebe39f07c6c9533dcfb0a20
Nicolás Cabello <cabellonic@gmail.com>
2022-06-09 16:26:57 -0300

    update: Use TsuiniPlayer in App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import UserProvider from './common/contexts/user.context';
+import TsuiniPlayer from '@components/TsuiniPlayer';
 import PublicRoutes from '@routes/public';
 import PrivateRoutes from '@routes/private';
 
@@ -11,6 +12,7 @@ const App = () => {
 				<Route path='/*' element={<PublicRoutes />} />
 				<Route path='/dashboard/*' element={<PrivateRoutes />} />
 			</Routes>
+			<TsuiniPlayer />
 		</UserProvider>
 	);
 };
