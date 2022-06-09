6b2a93bc772acced3bcc785a472adf4297086db4
Nicolás Cabello <cabellonic@gmail.com>
2022-06-09 16:56:00 -0300

    refactor: Merge the player and track service into a new one called spotify

--- src/common/services/spotify.service.ts
+++ src/common/services/spotify.service.ts
@@ -39,3 +39,38 @@ export const toggleRepeat = async (repeatMode: number): Promise<void> => {
 	);
 	if (response.error) throw response.error;
 };
+
+export const checkSaveStatus = async (songId: string): Promise<boolean> => {
+	const response = await api.get(
+		`${SPOTIFY_API_URL}/me/tracks/contains?ids=${songId}`,
+		{ withCredentials: false }
+	);
+	if (response.error) throw response.error;
+	return response.data[0];
+};
+
+export const saveTrack = async (songId: string): Promise<void> => {
+	const response = await api.update(
+		`${SPOTIFY_API_URL}/me/tracks`,
+		{ ids: [songId] },
+		{ withCredentials: false }
+	);
+	if (response.error) throw response.error;
+};
+
+export const removeTrack = async (songId: string): Promise<void> => {
+	const response = await api.remove(
+		`${SPOTIFY_API_URL}/me/tracks?ids=${songId}`,
+		{ withCredentials: false }
+	);
+	if (response.error) throw response.error;
+};
+
+export const getTrack = async (songId: string): Promise<any> => {
+	const response = await api.get(`/tracks/${songId}`, {
+		withCredentials: false,
+	});
+	if (response.error) throw response.error;
+	if (response.data) return response.data;
+	return null;
+};
