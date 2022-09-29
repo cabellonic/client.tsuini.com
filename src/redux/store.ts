c816959b8d577422c580639ccdca777572582946
Nicolás Cabello <cabellonic@gmail.com>
2022-09-29 00:21:19 -0300

    feature: Set up Redux store with the RTK API

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './states/user';
+import { baseApi } from '@/services';
 
 export const AppStore = configureStore({
 	reducer: {
-		user: userReducer,
+		[baseApi.reducerPath]: baseApi.reducer,
 	},
+	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof AppStore.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof AppStore.dispatch;
