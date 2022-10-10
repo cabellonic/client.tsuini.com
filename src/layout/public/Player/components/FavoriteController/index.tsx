62fdb2390a60799c1ca23ceb31e550a63ce56c40
Nicolás Cabello <cabellonic@gmail.com>
2022-10-09 21:51:17 -0300

    update: Prepare Favorite controller for future changes

--- src/layout/public/Player/components/FavoriteController/index.tsx
+++ src/layout/public/Player/components/FavoriteController/index.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import { useContext, useEffect, useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+// Context
+import { PlayerContext } from '../../context/player.context';
 // Styles
 import styles from './index.module.scss';
 
 type Props = {};
 
 const FavoriteController: React.FC<Props> = () => {
-	const [liked, setLiked] = React.useState(false);
+	const { currentTrack } = useContext(PlayerContext);
+	const [liked, setLiked] = useState(false);
 
 	const handleLike = () => setLiked(p => !p);
+
+	useEffect(() => {
+		if (!currentTrack) return;
+	}, [currentTrack]);
+
+	if (!currentTrack) return null;
 
 	return (
 		<IconButton sx={{ color: 'white' }} onClick={handleLike}>
