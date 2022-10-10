7f93817a0c35e454c3cd40c59e75d02409229b1e
Nicolás Cabello <cabellonic@gmail.com>
2022-10-09 21:32:25 -0300

    fix: Remove space in time format string

--- src/utils/format.utils.ts
+++ src/utils/format.utils.ts
@@ -1,10 +1,12 @@
 export const msToTimeFormat = (ms: number | undefined) => {
-	if (!ms) return '0:00';
+	if (!ms) return '00:00';
 	const seconds = Math.floor((ms / 1000) % 60);
 	const minutes = Math.floor((ms / (1000 * 60)) % 60);
 	const hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
 
-	return `${hours ? `${hours}:` : ''}
-					${minutes < 10 ? `0${minutes}` : minutes}:
-					${seconds < 10 ? `0${seconds}` : seconds}`;
+	return (
+		`${hours ? `${hours}:` : ''}` +
+		`${minutes < 10 ? `0${minutes}` : minutes}:` +
+		`${seconds < 10 ? `0${seconds}` : seconds}`
+	);
 };
