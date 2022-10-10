a32c6578d84c4cfe4cd2c715cc3ff51fd0943de4
Nicolás Cabello <cabellonic@gmail.com>
2022-10-09 21:54:26 -0300

    feature: User can play/pause track

--- src/layout/public/Player/components/PlayController/index.tsx
+++ src/layout/public/Player/components/PlayController/index.tsx
@@ -1,30 +1,45 @@
-import React from 'react';
+import { useContext } from 'react';
 import IconButton from '@mui/material/IconButton';
 import PauseRounded from '@mui/icons-material/PauseRounded';
 import PlayArrowRounded from '@mui/icons-material/PlayArrowRounded';
+// Context
+import { PlayerContext } from '../../context/player.context';
+// Services
+import { useSetPlaybackPauseMutation, useSetPlaybackPlayMutation } from '@/services';
 // Styles
 import styles from './index.module.scss';
 
 type Props = {};
 
 const PlayController: React.FC<Props> = () => {
-	const [isPlaying, setIsPlaying] = React.useState(false);
+	const { playbackState } = useContext(PlayerContext);
+	const [setPlay, { isLoading: isSettingPlay }] = useSetPlaybackPlayMutation();
+	const [setPause, { isLoading: isSettingPause }] = useSetPlaybackPauseMutation();
 
-	const togglePlay = () => setIsPlaying(p => !p);
+	const togglePlay = async () => {
+		if (!playbackState || isSettingPlay || isSettingPause) return;
+		try {
+			if (playbackState.paused) await setPlay();
+			else await setPause();
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	return (
-		<span className={styles.play_controller}>
+		<span className={styles.play_controller} data-active={Boolean(playbackState)}>
 			<IconButton
-				aria-label={isPlaying ? 'pause' : 'play'}
+				aria-label={playbackState?.paused ? 'pause' : 'play'}
 				onClick={togglePlay}
+				disabled={!Boolean(playbackState)}
 				classes={{
 					root: styles.play_controller_button_root,
 				}}
 			>
-				{isPlaying ? (
-					<PauseRounded className={styles.play_controller_button_icon} />
-				) : (
+				{!playbackState || playbackState.paused ? (
 					<PlayArrowRounded className={styles.play_controller_button_icon} />
+				) : (
+					<PauseRounded className={styles.play_controller_button_icon} />
 				)}
 			</IconButton>
 		</span>
