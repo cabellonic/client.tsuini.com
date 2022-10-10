e30a4efdec9f4c5bb70097312ca4212bdaa303b2
Nicolás Cabello <cabellonic@gmail.com>
2022-10-09 21:52:09 -0300

    update: Lyrics controller icon is disabled until currentTrack is loaded

--- src/layout/public/Player/components/LyricsController/index.tsx
+++ src/layout/public/Player/components/LyricsController/index.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import { useContext } from 'react';
 import IconButton from '@mui/material/IconButton';
 import MicExternalOnIcon from '@mui/icons-material/MicExternalOn';
+// Context
+import { PlayerContext } from '../../context/player.context';
 // Styles
 import styles from './index.module.scss';
 
 type Props = {};
 
 const LyricsController: React.FC<Props> = () => {
+	const { currentTrack } = useContext(PlayerContext);
+
+	const handleLyrics = () => {
+		if (!currentTrack) return;
+		console.log('Open song lyrics...');
+	};
+
 	return (
-		<IconButton sx={{ color: 'white' }}>
-			<MicExternalOnIcon sx={{ fontSize: 20 }} />
-		</IconButton>
+		<span className={styles.lyrics_controller} data-active={Boolean(currentTrack)} onClick={handleLyrics}>
+			<IconButton disabled={!Boolean(currentTrack)} sx={{ color: 'white' }} aria-label='Ver letra'>
+				<MicExternalOnIcon sx={{ fontSize: 20 }} />
+			</IconButton>
+		</span>
 	);
 };
 
