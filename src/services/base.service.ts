9d2cdb55e5d1e1d9f8d4be423e3dd32066a171cf
Nicolás Cabello <cabellonic@gmail.com>
2023-01-12 21:45:55 -0300

    update: Add tags to baseApi

--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -3,5 +3,5 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
 	baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URI, credentials: 'include' }),
 	endpoints: () => ({}),
-	tagTypes: ['Artist'],
+	tagTypes: ['Artist', 'Song', 'Album'],
 });
