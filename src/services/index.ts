ba2c8eedf296e5e3b3c99575d2a151e467f6cec1
Nicolás Cabello <cabellonic@gmail.com>
2023-01-12 22:52:09 -0300

    update: Add song service to services barrel

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,4 +3,5 @@ export * from './artist.service';
 export * from './auth.service';
 export * from './base.service';
 export * from './player.service';
+export * from './song.service';
 export * from './users.service';
