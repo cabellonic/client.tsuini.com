511a651dfb098c37c380da6387a433462c4cba19
Nicolás Cabello <cabellonic@gmail.com>
2023-01-12 22:55:19 -0300

    update: Add song page to pages barrel

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
 export { default as HomePage } from './home';
 export { default as ArtistPage } from './artist';
 export { default as AlbumPage } from './album';
+export { default as SongPage } from './song';
 export { default as NotFoundPage } from './not-found';
