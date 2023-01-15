b87ec4718ead96b0a50600f78de2e0eee8d1dcfc
Nicolás Cabello <cabellonic@gmail.com>
2023-01-15 00:27:54 -0300

    update: Show user playlists on home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import { useGetAlbumsQuery } from '@/services';
+import { useGetMyPlaylistsQuery } from '@/services/playlist.service';
 import { Link } from 'react-router-dom';
 // Tabla
 import Paper from '@mui/material/Paper';
@@ -9,31 +10,47 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 
-const SongPage = () => {
+const HomePage = () => {
 	const { data: albums, isLoading } = useGetAlbumsQuery();
+	const { data: playlists, isLoading: isLoadingPlaylist } = useGetMyPlaylistsQuery();
 
-	if (isLoading) return <h2>Loading...</h2>;
+	if (!playlists) return <h2>No existe el artista...</h2>;
+
+	if (isLoading || isLoadingPlaylist) return <h2>Loading...</h2>;
 
 	return (
-		<TableContainer component={Paper}>
-			<Table sx={{ minWidth: 650 }} aria-label='simple table'>
-				<TableHead>
-					<TableRow>
-						<TableCell>Albums</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{albums?.map(album => (
-						<TableRow key={album.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-							<TableCell>
-								<Link to={`/albums/${album.id}`}>{album.name}</Link>
-							</TableCell>
+		<>
+			<section>
+				<h3>Playlists</h3>
+				{playlists.map(playlist => {
+					return (
+						<div key={playlist.id}>
+							<Link to={`/playlists/${playlist.id}`}>{playlist.name}</Link>
+							<hr />
+						</div>
+					);
+				})}
+			</section>
+			<TableContainer component={Paper}>
+				<Table sx={{ minWidth: 650 }} aria-label='simple table'>
+					<TableHead>
+						<TableRow>
+							<TableCell>Albums</TableCell>
 						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-		</TableContainer>
+					</TableHead>
+					<TableBody>
+						{albums?.map(album => (
+							<TableRow key={album.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+								<TableCell>
+									<Link to={`/albums/${album.id}`}>{album.name}</Link>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</>
 	);
 };
 
-export default SongPage;
+export default HomePage;
